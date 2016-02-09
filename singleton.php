4545fb8bf32373643bfcbd832bcd684bed66fc6e
mitayshh <mitayshh.dhaggai@gmail.com>
2016-02-09 10:32:01 -0500

    Singleton in PHP

---
+++ singleton.php
@@ -0,0 +1,27 @@
+<?php
+class singleton
+{
+	private static $instance;
+
+	private function __clone()
+	{
+
+	}
+
+	private function singleton()
+	{
+
+	}
+	public static function getinstance()
+	{
+		if($instance === null)
+		{
+			$instance = new singleton();
+		}
+		return $instance;
+	}
+}
+$first = singleton::getinstance();
+var_dump($first);
+
+?>
